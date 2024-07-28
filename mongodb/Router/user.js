c1ef77de2b97acbcd9588935fdac9a77ebfea264
HieuNguyen3112 <zunpom2209@gmail.com>
2024-07-28 23:51:04 +0700

    Phan quyen dang nhap admin va token

--- mongodb/Router/user.js
+++ mongodb/Router/user.js
@@ -1,11 +1,12 @@
+const middlewareController = require("../controllers/middlewareController");
 const userController = require("../controllers/userController");
 
 const router = require("express").Router();
 
 //get all users
-router.get("/", userController.getAllUser);
+router.get("/", middlewareController.verifyToken, userController.getAllUser);
 
 //delete user
-router.delete("/:id", userController.deleteUser);
+router.delete("/:id", middlewareController.verifyTokenAndAdminAuth, userController.deleteUser);
 
 module.exports = router;
